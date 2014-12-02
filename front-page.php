6019d2c2c5d62388c19227a3381d7b229dec8c97
Laura Eagin <lauraeagin@gmail.com>
2014-12-02 13:06:18 -0600

    Styling front page further

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
 				<div class="col-md-12">
 
 				<?php if( get_field( "mission_statement" ) ): ?>
-					<h2 class="white"><?php the_field('mission_statement'); ?></h2>
+					<h2 class="white text-center"><?php the_field('mission_statement'); ?></h2>
 						<?php endif;
 		        ?>
 			    </div>
@@ -21,7 +21,7 @@
 			<div class="col-md-12">
 
 		<?php if( get_field( "title_above_our_work_showcase" ) ): ?>
-			<h2><?php the_field('title_above_our_work_showcase'); ?></h2>
+			<h2 class="text-center"><?php the_field('title_above_our_work_showcase'); ?></h2>
 				<?php endif;
         ?>
 
@@ -32,13 +32,13 @@
 
 				<?php dynamic_sidebar('homepage-portfolio'); ?>
 			<div class="col-md-4">
-				<h3>Example 1</h3>
+				<h3 class="text-center">Example 1</h3>
 			</div>
 			<div class="col-md-4">
-				<h3>Example 1</h3>
+				<h3 class="text-center">Example 1</h3>
 			</div>
 			<div class="col-md-4">
-				<h3>Example 1</h3>
+				<h3 class="text-center">Example 1</h3>
 			</div>
 		</div>
 
@@ -49,37 +49,40 @@
 			<div class="col-md-12">
 
         <?php if( get_field( "title_above_our_bios" ) ): ?>
-			<h2><?php the_field('title_above_our_bios'); ?></h2>
+			<h2 class="text-center"><?php the_field('title_above_our_bios'); ?></h2>
 				<?php endif;
         ?>
          <?php if( get_field( "our_bio_paragraph" ) ): ?>
 					<p><?php the_field('our_bio_paragraph'); ?></p>
 		 		<?php endif;
 		?>
-    		</div>
+    		
+
 			<div class="col-md-4">
-				<h3>Laura Eagin</h3>
+				<h3 class="text-center">Laura Eagin</h3>
 		         <?php if( get_field( "laura_bio_photo" ) ): ?>
-					<li><a href="/about/#laura"><img src="<?php the_field('laura_bio_photo'); ?>" alt="Laura Eagin web designer" width="150"/></a></li>
+					<li class="img-circle"><a href="/about/#laura"><img src="<?php the_field('laura_bio_photo'); ?>" class="img-circle" alt="Laura Eagin web designer" width="200"/></a></li>
 		 				<?php endif;
 		        ?>
     		</div>
 			<div class="col-md-4">
-				<h3>Leeann Drees</h3>
+				<h3 class="text-center">Leeann Drees</h3>
 		        <?php if( get_field( "leeann_bio_photo" ) ): ?>
-					<li><a href="/about/#leeann"><img src="<?php the_field('laura_bio_photo'); ?>" alt="Leeann Drees web developer" width="150"/></a></li>
+					<li class="img-circle"><a href="/about/#leeann"><img src="<?php the_field('leeann_bio_photo'); ?>" class="img-circle" alt="Leeann Drees web developer" width="200"/></a></li>
 		 				<?php endif;
 		        ?>
 		    </div>
 
 		    <div class="col-md-4">
-				<h3>&amp; Company</h3>
+				<h3 class="text-center">&amp; Company</h3>
 		         <?php if( get_field( "co_bio_photo" ) ): ?>
-					<li><a href="/about/"><img src="<?php the_field('co_bio_photo'); ?>" alt="web development team" width="150"/></a></li>
+					<li><a href="/about/"><img src="<?php the_field('co_bio_photo'); ?>" class="img-circle" alt="web development team" width="200"/></a></li>
 		 				<?php endif;
 		        ?>
     		</div>
 
+    		</div>
+ 
     </section>
 
 		<section id="call-to-action" class="homepage-cta">			
@@ -88,14 +91,19 @@
 				<?php if( get_field( "call_to_action_title" ) ): ?>
 					<h3><?php the_field('call_to_action_title'); ?></h3>
  				<?php endif;
- 		?>
-
+ 				?>
+				<?php if( get_field( "call_to_action_additional_text" ) ): ?>
+					<p><?php the_field('call_to_action_additional_text'); ?></p>
+ 				<?php endif;
+ 				?>
+ 		
+</div>
         <?php if( get_field( "call_to_action_link" ) ): ?>
-			<a class="call-to-action-button" href="<?php the_field('call_to_action_link'); ?>" title="Work with us">Work with us</a>
+			<a class="call-to-action-button btn btn-primary" href="<?php the_field('call_to_action_link'); ?>" title="Work with us">Work with us</a>
  				<?php endif;
         ?>			
 				</div>
-			</div>
+			
 		</section>
 	</div>
 </div>
