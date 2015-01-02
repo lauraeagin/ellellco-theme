a2bdeec09f47397bc9b04b9de414f982f813457c
LeeannDrees <leeann.drees@gmail.com>
2015-01-02 15:12:08 -0500

    added Portfolio custom post type to categories & tags

--- functions.php
+++ functions.php
@@ -15,3 +15,16 @@ register_sidebar(array(
 		'before_title' => '<h4>',
 		'after_title' => '</h4>'
 	));
+
+//Add Portfolio Custom Post Type to Category & Tag Archives
+function add_custom_types_to_tax( $query ) {
+if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+// Get all your post types
+$post_types = get_post_types();
+
+$query->set( 'post_type', $post_types );
+return $query;
+}
+}
+add_filter( 'pre_get_posts', 'add_custom_types_to_tax' );
