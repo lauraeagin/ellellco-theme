9f2f55dedb6470059c74e7663c0873aa51b3b02c
LeeannDrees <leeann.drees@gmail.com>
2015-01-03 16:38:39 -0500

    fixing alt tag

--- parts/loop_portfolio.php
+++ parts/loop_portfolio.php
@@ -1,10 +1,11 @@
 <?php $loop = new WP_Query( array( 'post_type' => 'portfolio', 'posts_per_page' => 3 ) ); ?>
 		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		<?php $imgurl = get_field('homepage_thumbnail');
+					$title = get_the_title(); ?>
 			<div class="col-md-4">
 			<a href="<?php the_permalink(); ?>">
-				<?php if (get_field('homepage_thumbnail')) {
-					$imgurl = get_field('homepage_thumbnail');
-					echo '<img src="'.$imgurl.'" alt="'.the_title().'" class="homepage-thumbnail aligncenter">';
+				<?php if ($imgurl) {
+					echo '<img src="'.$imgurl.'" alt="'.$title.'" class="homepage-thumbnail aligncenter">';
 				}
 				 ?>
 			<!--<?php the_post_thumbnail('medium', array('class' => 'aligncenter')); ?>-->
