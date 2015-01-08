23e85ea4b23df49ae17e13664a468eaa722137cc
LeeannDrees <leeann.drees@gmail.com>
2015-01-08 15:17:59 -0500

    removed comment count on portfolio items

--- content-portfolio.php
+++ content-portfolio.php
@@ -9,7 +9,6 @@ $testimonial = get_field('testimonial');
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<!--<?php the_post_thumbnail('featured-full'); ?>-->
 		<div id="post-content">
-			<span id="post-comment-total"><?php comments_number('0', '1', '%'); ?></span>
 			<?php the_title('<h1 id="post-title">', '</h1>'); ?>
 			<?php the_content() ?>
 			<?php include("parts/site_button.php"); ?>
